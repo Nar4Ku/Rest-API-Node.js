689e186181fc7f1596b54821f4d4110c76ffa0e9
Nar4ku <lfpetreca02@gmail.com>
2018-07-22 19:13:52 -0300

    Crud Rest

--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controller/productsController');
 
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
+router.get('/tag/:tag', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
